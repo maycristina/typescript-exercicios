f0befd2056ae7f523683e9cf4d6d2d32fd5de725
maycristina <mayara.crst@gmail.com>
2022-01-04 18:08:47 -0300

    inicio-d-exercicio

---
+++ app/app.ts
@@ -0,0 +1,14 @@
+//Para um script funcionar dentro de outro e interagirem tem que ser exportado e importado
+
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+
+
+const controller = new NegociacaoController();
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+})
---
+++ app/models/negociacoes.ts
@@ -0,0 +1,19 @@
+import { Negociacao } from "./negociacao.js";
+
+export class Negociacoes {
+    private negociacoes: Negociacao [] = [];
+
+    adiciona (negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    lista(): readonly Negociacao[]{  
+        return [...this.negociacoes];
+    }
+}
+
+
+const negociacoes = new Negociacoes ();
+negociacoes.lista().forEach( n => {
+    n
+})
